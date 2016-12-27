49f525a2c156c2289e2aac8e60785a33f248e779
vasanthakumar subramanian <v-vsubramanian@expedia.com>
2016-12-27 10:04:10 -0800

    join fork

---
+++ src/streams/map/MapDemo.java
@@ -0,0 +1,18 @@
+package streams.map;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Created by vasanthakumarsubramanian on 12/25/16.
+ */
+public class MapDemo {
+    public static void main(String[] args) {
+        List<String> strList = Arrays.asList("1","2","3");
+        //maps the Integer(String) constructor on to each element of strList
+        List<Integer> intList = strList.stream().map(Integer::new).collect(Collectors.toList());
+        System.out.println(strList);
+        System.out.println(intList);
+    }
+}
