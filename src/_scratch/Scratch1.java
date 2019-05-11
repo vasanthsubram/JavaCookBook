5225d52a58dd80767f8172857345c43cb0241f97
vasanthsubramanian <v-vsubramanian@expedia.com>
2019-05-11 16:36:27 -0700

    sundry

--- src/_scratch/Scratch1.java
+++ src/_scratch/Scratch1.java
@@ -1,11 +1,7 @@
 package _scratch;
 
-/**
- * Created by vasanth on 6/29/17.
- */
+
 public class Scratch1 {
 
-    //Test 1
-    //Test 2
-    //Test 3
 }
+
---
+++ src/core/collection/sorting/ComparableExample.java
@@ -0,0 +1,48 @@
+package core.collection.sorting;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ComparableExample {
+
+  public static void main(String[] args){
+    List<Employee> list = new ArrayList<>();
+    list.add(new Employee(10));
+    list.add(new Employee(2));
+    list.add(new Employee(1));
+
+    System.out.println(list);
+
+    Collections.sort(list);
+
+    System.out.println(list);
+  }
+}
+
+
+class Employee implements Comparable<Employee> {
+
+  private Integer id;
+
+  public Employee(Integer id) {
+    this.id = id;
+  }
+
+  public Integer getId() {
+    return id;
+  }
+
+  @Override
+  public int compareTo(Employee o)
+  {
+    return this.getId().compareTo( o.getId() );
+  }
+
+  @Override
+  public String toString() {
+    return "Employee{" +
+          "id=" + id +
+          '}';
+  }
+}
